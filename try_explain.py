b759199b5f8e2211ea0920a2956049d55486b26b
pascal65536 <pascal65536@gmail.com>
2025-03-19 14:04:21 +0700

    90

--- try_explain.py
+++ try_explain.py
@@ -7,14 +7,18 @@ access_token = ret.get("access_token")
 assert len(access_token) == 331, "Error in `get_access_token`"
 
 # Загрузить книги
-# pth = "/media/pascal65536/1TB/Books/"
-# for root, dirs, files in os.walk(pth):
-#     for f in files:
-#         book_path = os.path.join(root, f)
-#         if book_path.endswith(".pdf"):
-#             ret = book_upload(book_path, access_token, govdatahub="govdatahub.ru")
-#             print(ret)
-#             print('-' * 20)
+pth = "/media/pascal65536/1TB/Books/"
+for root, dirs, files in os.walk(pth):
+    for f in files[-5:-1]:
+        ret = get_access_token("user1", "password1", govdatahub="govdatahub.ru")
+        access_token = ret.get("access_token")
+        assert len(access_token) == 331, "Error in `get_access_token`"    
+
+        book_path = os.path.join(root, f)
+        if book_path.endswith(".pdf"):
+            ret = book_upload(book_path, access_token, govdatahub="govdatahub.ru")
+            print(ret)
+            print('-' * 20)
 
 
 # Скачивание книги
